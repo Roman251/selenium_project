435320117aadc1942c7df16474bc765168e2e54e
Roman251 <romanregmi@hotmail.com>
2021-02-27 12:20:20 +0545

    Update ipo.py

--- selenium_project/ipo.py
+++ selenium_project/ipo.py
@@ -1,4 +1,34 @@
-//input[@class='select2-search__field']
-//input[@id='username']
-//input[@id='password']
-//button[@class='btn sign-in']
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+
+
+driver = webdriver.Chrome(executable_path="/Users/romanregmi/Desktop/selenium_project/driver/chromedriver")
+
+
+driver.get("https://meroshare.cdsc.com.np/#/login")
+
+wait = WebDriverWait(driver, 60)
+
+wait.until(ec.presence_of_element_located((By.XPATH, '//span[@class="select2-selection__placeholder"]'))).click()
+
+
+driver.find_element(By.XPATH, '//input[@class="select2-search__field"]').send_keys('GLOBAL IME CAPITAL LIMITED (11200)')
+driver.find_element(By.XPATH, '//input[@class="select2-search__field"]').send_keys(Keys.RETURN)
+
+driver.find_element(By.XPATH, '//input[@id="username"]').send_keys('') #input your username
+driver.find_element(By.XPATH, '//input[@id="password"]').send_keys('') #input your password
+driver.find_element(By.XPATH, '//button[@class="btn sign-in"]').click()
+
+time.sleep(3)
+
+driver.find_element(By.XPATH, '//i[@class="msi msi-asba"]').click()
+
+time.sleep(5)
+
+
+driver.quit()

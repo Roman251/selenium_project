408df9e287653f09b4309e52e32b85cc9d78379a
Roman251 <romanregmi@hotmail.com>
2021-02-27 12:26:30 +0545

    Update main.py

--- selenium_project/main.py
+++ selenium_project/main.py
@@ -7,19 +7,24 @@ print("enter the initials of the companies you would want to buy and sell stocks
 ta = ['UMHL','MBL']
 
 disp = input("display the charts? Y:Yes|N:No" )
-macd = input("use macd indicator to trade? Y:Yes|N:No")
-
-trade = input("would you like to trade?")
 
 if disp == 'Y' or disp == 'y':
     get_vis(ta)
 
+macd = input("use macd indicator to trade? Y:Yes|N:No")    
+    
 if macd == 'Y' or macd == 'y':
     print(get_data(ta))
+    
+
+trade = input("would you like to trade?")
 
 if trade == 'Y' or trade == 'y':
     action = input("Buy:Buy|Sell:Sell")
     user_name = input("username")
     password = input("password")
     shares = int(input("number of shares"))
-    buy_sell(action,user_name,password,shares,ta)
+    
+    
+    # buy_sell(action,user_name,password,shares,ta) --> this function has not been defined with shares and ta. 
+    
